4813d313f227cee4812d6f60255e03c57e98e6f5
Joel Berger <joel.a.berger@gmail.com>
2012-06-09 10:57:55 -0500

    one missed change from last commit

--- NumSim/MySim.pm
+++ NumSim/MySim.pm
@@ -28,7 +28,7 @@ class MySim {
     my $dt = $self->step;
     my $vx = $thing->vx;
 
-    my $force = sum map { $_->affect->($_, $thing, $self) } @{ $self->forces };
+    my $force = sum map { $_->affect->($_, $thing) } @{ $self->forces };
     my $acc = $force / ($thing->mass);
 
     $thing->vx( $vx + $acc * $dt );
