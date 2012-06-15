e8bd73e63d3ef6fc2cb8a70a6adca82a2a155f1a
Joel Berger <joel.a.berger@gmail.com>
2012-06-15 08:37:50 -0500

    move physical classes to the front of the example

--- NumSim/code/class0.pl
+++ NumSim/code/class0.pl
@@ -1,16 +1,16 @@
-class MyForce {
+use MooseX::RememberHistory;
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
 
+class MyForce {
   has 'strength' => ( isa => 'Num',  is => 'rw', default => 0 );
-  has 'affect'   => ( isa => 'CodeRef', is => 'ro', required => 1 );
-  
+  has 'affect'   => ( isa => 'CodeRef', is => 'ro', required => 1 ); 
 }
 
 class MyThing {
-
   has 'mass' => ( isa => 'Num', is => 'ro', required => 1 );
   has 'x'    => ( traits => [ 'RememberHistory' ], isa => 'Num', 
                   is => 'rw', default => 0 );
   has 'vx'   => ( traits => [ 'RememberHistory' ], isa => 'Num',
                   is => 'rw', default => 0 );
-
 }
